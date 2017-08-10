712961abbfba7b5abc36f21000c74b526cbdd647
Asaf Alima <asaf.alima@gmail.com>
2017-08-10 11:48:41 +0300

    Add wait until consul is ready to standalone consul

--- conf4j-consul/src/test/java/org/conf4j/consul/StandaloneConsul.java
+++ conf4j-consul/src/test/java/org/conf4j/consul/StandaloneConsul.java
@@ -6,12 +6,17 @@ import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.awaitility.Awaitility.await;
+
 public class StandaloneConsul {
 
     private static final Logger logger = LoggerFactory.getLogger(StandaloneConsul.class);
 
     private final GenericContainer consulContainer;
-    private Consul consul;
+    private AtomicReference<Consul> consul = new AtomicReference<>();
 
     public StandaloneConsul() {
         consulContainer = new GenericContainer("consul:0.9.0")
@@ -22,9 +27,17 @@ public class StandaloneConsul {
     public void start() {
         consulContainer.start();
         consulContainer.followOutput(new Slf4jLogConsumer(logger).withPrefix("consul"));
-        consul = Consul.builder()
-                .withUrl(getConsulUrl())
-                .build();
+        await("Consul is ready")
+                .atMost(10, TimeUnit.SECONDS)
+                .until(() -> {
+                    try {
+                        consul.set(Consul.builder().withUrl(getConsulUrl()).build());
+                        return true;
+                    } catch (Exception e) {
+                        logger.info("Failed to create consul client, probably consul is not ready yet", e);
+                        return false;
+                    }
+                });
     }
 
     public void stop() {
@@ -33,11 +46,11 @@ public class StandaloneConsul {
     }
 
     public Consul getConsul() {
-        if (consul == null) {
+        if (consul.get() == null) {
             throw new IllegalStateException("Standalone consul must be started before calling getConsul");
         }
 
-        return consul;
+        return consul.get();
     }
 
     public String getConsulUrl() {
